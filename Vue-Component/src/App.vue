470ec0bc44ae1789421be94c27f18671bd88f41b
ZokaHong <zoka105105@gmail.com>
2023-11-15 21:52:24 +0800

    Component_V1
    Vue Parent-Child component setup and data transmission

---
+++ Vue-Component/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, reactive} from 'vue';
+import Child from './components/Child.vue'
+
+const msg = ref('')
+
+const updateHandler = (newValue)=>{
+    console.log(newValue)
+}
+
+</script>
+
+<template>
+
+<input type="text" v-model="msg">
+<br/>
+<Child :data="msg" @update="updateHandler"/>
+
+
+</template>
+
+<style>
+
+</style>
---
+++ Vue-Component/src/components/Child.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { computed, watch } from 'vue';
+
+const emit = defineEmits(['update'])
+
+// defineProps(['data'])
+const props = defineProps({
+    data: {
+        type:String,
+        default:'',
+    }
+})
+
+const count = computed(()=>{
+    return props.data.length
+})
+
+watch( count,(newValue)=>{
+    emit('update', `from child: ${newValue}`)
+})
+
+</script>
+
+<template>
+
+<div>
+    字數: {{count}}
+    <!-- data: {{ data }} -->
+</div>
+
+
+</template>
+
+<style>
+
+</style>
