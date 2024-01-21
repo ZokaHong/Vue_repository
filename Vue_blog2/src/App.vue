8862ef5246ca7cd16be8f6ffa867c1cbabc7cfca
お茶 <zoka105105@gmail.com>
2024-01-21 19:03:06 +0800

    Vue_blog2_V2

--- Vue_blog2/src/App.vue
+++ Vue_blog2/src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-import BlogPost1 from './Component/BlogPost1.vue'
-import BlogPost2 from './Component/BlogPost2.vue'
-import BlogPost3 from './Component/BlogPost3.vue';
+import animation1 from './components/animation1.vue'
 </script>
 
 <template>
-    <BlogPost1></BlogPost1>
-    <BlogPost2></BlogPost2>
-    <BlogPost3></BlogPost3>
+ <animation1></animation1>
 </template>
+
+<style>
+
+</style>
--- Vue_blog2/src/main.js
+++ Vue_blog2/src/main.js
@@ -1,5 +1,25 @@
+/* Set up using Vue 3 */
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+/* import the bootstrap */
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap"
+
+
+/* import the fontawesome core (1) */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component (1) */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons (2) */
+import { faBars, faPhone, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faImage } from '@fortawesome/free-regular-svg-icons'
+/* add icons to the library (2) */
+library.add(faBars, faImage, faPhone, faMagnifyingGlass)
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
